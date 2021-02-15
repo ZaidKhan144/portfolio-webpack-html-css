5b2e5d62f8250952fa0e6e29a1c1b338201391b0
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-02-15 00:32:42 -0600

    Resume added

--- src/js/home.js
+++ src/js/home.js
@@ -4,16 +4,17 @@ function homeContent(){
     //delay home-content
 $(document).ready(function(){
     setTimeout(function(){
-      $('.home-content').slideUp( 300 ).delay( 400 ).fadeIn( 400 );;
+      $('.home-content').slideUp( 300 ).delay( 400 ).fadeIn( 400 );
     });
   });
   
 //delay title
   $(document).ready(function(){
     setTimeout(function(){
-      $('.title').slideUp( 300 ).delay( 600 ).fadeIn( 400 );;
+      $('.title').slideUp( 300 ).delay( 600 ).fadeIn( 400 );
     });
   });
+
 }
 
 export default homeContent
